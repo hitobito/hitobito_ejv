1d1649e77d0d8367f78c4e608c67e272241ffe6f
Matthias Viehweger <kronn@kronn.de>
2025-03-17 13:06:21 +0100

    Remove columns that are not needed by EJV

---
+++ db/migrate/20250314152945_remove_sbv_attributes_from_groups.rb
@@ -0,0 +1,9 @@
+class RemoveSbvAttributesFromGroups < ActiveRecord::Migration[7.1]
+  def change
+    remove_column :groups, :correspondence_language, :string, limit: 5
+    remove_column :groups, :besetzung, :string
+    remove_column :groups, :klasse, :string
+    remove_column :groups, :unterhaltungsmuisk, :string
+    remove_column :groups, :subventionen, :string
+  end
+end
