ec16866837d535853ec9e80bf54bc1e20955fb77
Matthias Viehweger <kronn@kronn.de>
2025-09-05 22:30:12 +0200

    Seed the correct verbandsstruktur in dev as well

--- db/seeds/development/0_groups.rb
+++ db/seeds/development/0_groups.rb
@@ -5,7 +5,28 @@
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_ejv.
 
+require Rails.root.join("db", "seeds", "support", "group_seeder")
 
-require Rails.root.join('db', 'seeds', 'support', 'group_seeder')
+Group::Root.seed_once(:parent_id, name: "Eidgenössischer Jodlerverband")
 
-Group::Root.seed_once(:parent_id, name: 'EJV')
+root_id = Group.where(name: "Eidgenössischer Jodlerverband").pluck(:id).first
+
+Group::Mitgliederverband.seed_once(:short_name, [
+  {parent_id: root_id, short_name: "BKJV", name: "Bernisch Kantonaler Jodlerverband"},
+  {parent_id: root_id, short_name: "NOSJV", name: "Nordostschweizerischer Jodlerverband"},
+  {parent_id: root_id, short_name: "NWSJV", name: "Nordwestschweizerischer Jodlerverband"},
+  {parent_id: root_id, short_name: "ZSJV", name: "Zentralschweizerischer Jodlerverband"},
+  {parent_id: root_id, short_name: "WSJV", name: "Westschweizerischer Jodlerverband"},
+  {parent_id: root_id, short_name: "EJV", name: "Ausländische Jodlerverbände"}
+])
+
+puts "Rebuilding nested set..."
+Group.rebuild!(false)
+puts "Moving Groups in alphabetical order..."
+Group.find_each do |group|
+  group.send(:move_to_alphabetic_position)
+rescue => e
+  puts e
+  puts group
+end
+puts "Done."
