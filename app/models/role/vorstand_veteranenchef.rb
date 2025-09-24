3d8ff8ac37b1be82d11a000a9988660cca90b6fb
Matthias Viehweger <kronn@kronn.de>
2025-09-24 14:00:27 +0200

    Grant Mitgliederverwalter more appropriate rights

--- app/models/role/vorstand_veteranenchef.rb
+++ app/models/role/vorstand_veteranenchef.rb
@@ -5,25 +5,6 @@
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_ejv.
 
-# == Schema Information
-#
-# Table name: roles
-#
-#  id         :integer          not null, primary key
-#  person_id  :integer          not null
-#  group_id   :integer          not null
-#  type       :string(255)      not null
-#  label      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
-#  deleted_at :datetime
-#
-
-#  Copyright (c) 2012-2013, Puzzle ITC GmbH. This file is part of
-#  hitobito_generic and licensed under the Affero General Public License version 3
-#  or later. See the COPYING file at the top-level directory or at
-#  https://github.com/hitobito/hitobito_generic.
-
 class Role::VorstandVeteranenchef < Role
-  self.permissions = [:layer_and_below_read]
+  self.permissions = [:layer_and_below_full]
 end
