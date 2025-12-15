32ea11f7a7649719ccd41c4841dd461faa9e13e1
Matthias Viehweger <kronn@kronn.de>
2025-12-15 23:01:06 +0100

    Active export for groups and roles

--- spec/domain/jodlerfest_export_spec.rb
+++ spec/domain/jodlerfest_export_spec.rb
@@ -22,6 +22,10 @@ describe JodlerfestExport do
   end
 
   it "#run" do
+    expect(export).to receive(:send_data).with("adressenstamm", any_args)
+    expect(export).to receive(:send_data).with("gruppen", any_args)
+    expect(export).to receive(:send_data).with("gruppenmitglieder", any_args)
+
     export.run
   end
 
